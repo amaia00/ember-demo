d157051161eae270b5c61765a5e402419373c122
Amaia Nazabal <amaia.nazabal-ruiz-diaz@etu.univ-lyon1.fr>
2017-03-23 01:36:29 +0100

    On enlève le console log.

--- app/helpers/moyene-annee.js
+++ app/helpers/moyene-annee.js
@@ -1,42 +1,23 @@
 import Ember from 'ember';
 
 export function moyeneAnnee(params,{param1}/*, hash*/) {
-  var moyenne=0;
-  console.log("param1=");
+  let moyenne = 0;
 
-  console.log(param1.content.currentState.length);
-  console.log("Params"+params);
-  for(var i=0 ; i<param1.content.currentState.length ; i++){
-    console.log("moyenne=");
-    console.log(param1.content);
-    console.log(param1.content.canonicalState[i]._data.cc1);
-    console.log("------------------------------------------");
+  for(let i=0 ; i < param1.content.currentState.length ; i++){
 
-    console.log(param1.content.currentState[i]._data.ccf);
-    console.log(param1.content.currentState[i]._data.moyenne);
     if(param1.content.currentState[i].cc1<0 && param1.content.currentState[i].cc2<-1){
       return -1;
     }
     else if(param1.content.currentState[i].cc2===-1 && param1.content.currentState[i].cc1>=0){
-      moyenne=moyenne+0.5*param1.content.currentState[i].cc1+0.5*param1.content.currentState[i].ccf;
+      moyenne = moyenne + 0.5 * param1.content.currentState[i].cc1 + 0.5 * param1.content.currentState[i].ccf;
     }
     else{
 
-      moyenne=moyenne+0.25*param1.content.currentState[i]._data.cc1+0.25*param1.content.currentState[i].cc2+0.5*param1.content.currentState[i].ccf;
+      moyenne = moyenne + 0.25 * param1.content.currentState[i]._data.cc1 + 0.25 *
+        param1.content.currentState[i].cc2 + 0.5 * param1.content.currentState[i].ccf;
     }
-
-    //console.log(param1.content.currentState[i]);
-    console.log("moyenne=");
-    console.log(param1.content);
-    console.log(param1.content.canonicalState[i]._data.cc1);
-    console.log("------------------------------------------");
-
-    console.log(param1.content.currentState[i]._data.ccf);
-    console.log(param1.content.currentState[i]._data.moyenne);
   }
-  console.log("------------------------------------------");
 
-  //console.log(" note"+param1.content.canonicalState[3]._data);
   return moyenne/param1.content.currentState.length;
 }
 
